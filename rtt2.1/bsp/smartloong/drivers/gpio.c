1492818ef762b9e4bf34a570d5d59bd39d12b446
chinesebear <nuptbear@gmail.com>
2017-06-19 13:33:07 +0800

    （编译）为GPIO添加中断配置接口

--- rtt2.1/bsp/smartloong/drivers/gpio.c
+++ rtt2.1/bsp/smartloong/drivers/gpio.c
@@ -26,78 +26,26 @@ KEY3=GPIO86
 
 */
 struct rt_device gpio[GPIO_MAX];
-#if 0
-rt_uint32_t gpio_num[GPIO_MAX] = {0,1,2,3,4,5,48,49,50,51,52,53,87,88,89,90,91};
-char* gpio_string[GPIO_MAX] = {"GPIO0","GPIO1","GPIO2","GPIO3","GPIO4","GPIO5",\
-	"GPIO48","GPIO49","GPIO50","GPIO51","GPIO52","GPIO53",\
-	"GPIO87","GPIO88","GPIO89","GPIO90","GPIO91"};
-#endif
+
 rt_uint32_t gpio_num[8] = {50,51,52,53,54,92,85,86};
 char* gpio_string[8] = {"LED1","LED2","LED3","LED4","LED5",\
 	"KEY1","KEY2","KEY3"};
 
+void key1_irqhandler(int irqno, void *param)
+{
 
-rt_err_t rt_gpio_num(rt_device_t dev, rt_uint32_t * base_addr, rt_uint32_t * ctlbit_pos);
-
-
-static rt_err_t rt_gpio_init(rt_device_t dev)
-{
-	RT_ASSERT(dev != RT_NULL);
-	
-	
-	return RT_EOK;
-}
-static rt_err_t rt_gpio_open(rt_device_t dev, rt_uint16_t oflag)
-{
-	RT_ASSERT(dev != RT_NULL);
-	return RT_EOK;
-}
-static rt_err_t rt_gpio_close(rt_device_t dev)
-{
-	RT_ASSERT(dev != RT_NULL);
-
-	return RT_EOK;
-}
-static rt_size_t rt_gpio_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size)
-{
-	RT_ASSERT(dev != RT_NULL);
-	rt_uint32_t p_gpio_base = 0;
-	rt_uint32_t p_ctlbit_pos =0;
-	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
-	rt_gpio_num(dev, &p_gpio_base,&p_ctlbit_pos);
-	//rt_kprintf("p_gpio_base=0x%x,p_ctlbit_pos=%d\n",p_gpio_base,p_ctlbit_pos);
-	//rt_kprintf("GPIO_EN(p_gpio_base) is 0x%x\n",GPIO_EN(p_gpio_base));
-	if (!(GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
-		rt_kprintf("GPIO is not read mode \n");
-	*p_buf=(GPIO_IN(p_gpio_base)&(1<<p_ctlbit_pos))==0?0:1;
-	//rt_kprintf("GPIO input is %d\n",*p_buf);
-	
-	return 1;
-}
-static rt_size_t rt_gpio_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size)
-{
-	RT_ASSERT(dev != RT_NULL);
-	rt_uint32_t p_gpio_base = 0;
-	rt_uint32_t p_ctlbit_pos =0;
-	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
-	//rt_uint32_t  p_value= *p_buf ;
-	rt_gpio_num(dev, &p_gpio_base,&p_ctlbit_pos);
-	//rt_kprintf("p_gpio_base=0x%x,p_ctlbit_pos=%d\n",p_gpio_base,p_ctlbit_pos);
-	//rt_kprintf("GPIO_EN(p_gpio_base) is 0x%x\n",GPIO_EN(p_gpio_base));
-	if ((GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
-		rt_kprintf("GPIO is not write mode \n");
-	//rt_kprintf("GPIO_OUT(p_gpio_base) is 0x%x\n",GPIO_OUT(p_gpio_base));
-	if( *p_buf  == 1)
-		GPIO_OUT(p_gpio_base) |= (1<<p_ctlbit_pos);
-	else if(*p_buf  == 0)
-		GPIO_OUT(p_gpio_base) &= ~(1<<p_ctlbit_pos);
-	else
-		rt_kprintf("GPIO output value(%d) is wrong \n",*p_buf);
-	//rt_kprintf("GPIO_OUT(p_gpio_base) is 0x%x now\n",GPIO_OUT(p_gpio_base));
-	return 1;
 }
 
-rt_err_t rt_gpio_num(rt_device_t dev,  rt_uint32_t * base_addr, rt_uint32_t * ctlbit_pos)
+void key2_irqhandler(int irqno, void *param)
+{
+
+}
+
+void key3_irqhandler(int irqno, void *param)
+{
+
+}
+rt_err_t gpio_ctrl_num(rt_device_t dev,  rt_uint32_t * base_addr, rt_uint32_t * ctlbit_pos)
 {
 	RT_ASSERT(dev != RT_NULL);
 	rt_uint32_t * p_tmp = (rt_uint32_t *)dev->user_data;
@@ -130,15 +78,136 @@ rt_err_t rt_gpio_num(rt_device_t dev,  rt_uint32_t * base_addr, rt_uint32_t * ct
 	}
 	else
 		return RT_EIO;
+	return RT_EOK;
 }
+
+static rt_err_t gpio_int_set(rt_device_t dev, int trigmethod, int edge, int level,char *isrName,rt_isr_handler_t handler)
+{
+	RT_ASSERT(dev != RT_NULL);
+	rt_uint32_t int_base;
+	int setbit;
+	rt_uint32_t * p_tmp = (rt_uint32_t *)dev->user_data;
+	rt_uint32_t p_gpio_num = *p_tmp;
+	if (p_gpio_num >=96 && p_gpio_num <= 105)
+	{
+		int_base = LS1C_INT1_BASE;
+		setbit = p_gpio_num - 74;
+	}
+	else if(p_gpio_num >=0 && p_gpio_num <= 31)
+	{
+		int_base = LS1C_INT2_BASE;
+		setbit = p_gpio_num;
+	}
+	else if(p_gpio_num >=32 && p_gpio_num <= 63)
+	{
+		int_base = LS1C_INT3_BASE;
+		setbit = p_gpio_num - 32;
+	}
+	else if(p_gpio_num >=64 && p_gpio_num <= 95)
+	{
+		int_base = LS1C_INT4_BASE;
+		setbit = p_gpio_num - 64;
+	}
+	else
+		return RT_EIO;
+	
+	//int trigmethod
+	switch(trigmethod)
+	{
+		case GPIO_TRIG_EDGE:
+			LS1C_INT_EDGE(int_base) |= (1<<setbit);
+			if(edge == GPIO_POSEDGE)
+			{
+				LS1C_INT_POL(int_base) |= (1<<setbit);
+			}
+			else if(edge == GPIO_NEGEDGE)
+			{
+				LS1C_INT_POL(int_base) &= ~(1<<setbit);
+			}
+			else
+				rt_kprintf("GPIO int edge err\r\n");
+			break;
+		case GPIO_TRIG_LEVEL:
+			LS1C_INT_EDGE(int_base) &= ~(1<<setbit);
+			if(edge == GPIO_HIGH_LEVEL)
+			{
+				LS1C_INT_POL(int_base) |= (1<<setbit);
+			}
+			else if(edge == GPIO_LOW_LEVEL)
+			{
+				LS1C_INT_POL(int_base) &= ~(1<<setbit);
+			}
+			else
+				rt_kprintf("GPIO int level err\r\n");
+			break;
+		default:
+			rt_kprintf("GPIO trig method err\r\n");
+			break;
+	}
+	rt_hw_interrupt_install(LS1C_GPIO_IRQ(p_gpio_num),handler,RT_NULL,isrName);
+	LS1C_INT_EN(int_base) |= (1<<setbit);
+	return RT_EOK;
+}
+
+
+static rt_err_t rt_gpio_init(rt_device_t dev)
+{
+	RT_ASSERT(dev != RT_NULL);
+	
+	
+	return RT_EOK;
+}
+static rt_err_t rt_gpio_open(rt_device_t dev, rt_uint16_t oflag)
+{
+	RT_ASSERT(dev != RT_NULL);
+	return RT_EOK;
+}
+static rt_err_t rt_gpio_close(rt_device_t dev)
+{
+	RT_ASSERT(dev != RT_NULL);
+
+	return RT_EOK;
+}
+static rt_size_t rt_gpio_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size)
+{
+	RT_ASSERT(dev != RT_NULL);
+	rt_uint32_t p_gpio_base = 0;
+	rt_uint32_t p_ctlbit_pos =0;
+	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
+	gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos);
+	if (!(GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
+		rt_kprintf("GPIO is not read mode \n");
+	*p_buf=(GPIO_IN(p_gpio_base)&(1<<p_ctlbit_pos))==0?0:1;
+	
+	return 1;
+}
+static rt_size_t rt_gpio_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size)
+{
+	RT_ASSERT(dev != RT_NULL);
+	rt_uint32_t p_gpio_base = 0;
+	rt_uint32_t p_ctlbit_pos =0;
+	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
+	gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos);
+	if ((GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
+		rt_kprintf("GPIO is not write mode \n");
+	if( *p_buf  == 1)
+		GPIO_OUT(p_gpio_base) |= (1<<p_ctlbit_pos);
+	else if(*p_buf  == 0)
+		GPIO_OUT(p_gpio_base) &= ~(1<<p_ctlbit_pos);
+	else
+		rt_kprintf("GPIO output value(%d) is wrong \n",*p_buf);
+	return 1;
+}
+
+
 
 rt_err_t  rt_gpio_control(rt_device_t dev, rt_uint8_t cmd, void *args)
 {
 	RT_ASSERT(dev != RT_NULL);
 	rt_uint32_t p_gpio_base = 0;
 	rt_uint32_t p_ctlbit_pos =0;
-	
-	if(rt_gpio_num(dev, &p_gpio_base,&p_ctlbit_pos) == RT_EIO)
+	gpio_int_config_t *config;
+	if(gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos) == RT_EIO)
 		return RT_EIO;
 	
 	switch(cmd)
@@ -148,6 +217,10 @@ rt_err_t  rt_gpio_control(rt_device_t dev, rt_uint8_t cmd, void *args)
 			break;
 		case GPIO_WRITE_MODE :
 			GPIO_EN(p_gpio_base) &= ~(1<<p_ctlbit_pos);//0=out
+			break;
+		case GPIO_INT_MODE :
+			config =(gpio_int_config_t *) args;
+			gpio_int_set(dev,config->trigmethod,config->edge,config->level,config->isrName,config->handler);
 			break;
 		default: break;
 	}
--- rtt2.1/bsp/smartloong/drivers/gpio.h
+++ rtt2.1/bsp/smartloong/drivers/gpio.h
@@ -17,7 +17,24 @@
 
 #define  GPIO_READ_MODE     (0)
 #define  GPIO_WRITE_MODE   (1)
+#define  GPIO_INT_MODE   	(2)
 
+#define GPIO_TRIG_EDGE		1
+#define GPIO_TRIG_LEVEL		0
+
+#define GPIO_POSEDGE		1
+#define GPIO_NEGEDGE		0
+
+#define GPIO_HIGH_LEVEL    1
+#define GPIO_LOW_LEVEL     0
+typedef struct Gpio_Int_Config_t
+{
+	int trigmethod;
+	int edge;
+	int level;
+	char *isrName;
+	rt_isr_handler_t handler;
+}gpio_int_config_t;
 void rt_hw_gpio_init(void);
 
 
